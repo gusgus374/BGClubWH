321b24f2be0a628f94b2a72f71ca7820ea3794e7
gusgus374 <gusalvarez374@gmail.com>
2025-02-06 14:58:19 -0500

    updated names

--- FootyLab.py
+++ FootyLab.py
@@ -30,8 +30,8 @@ if "user" not in st.session_state:
 #if "password" not in st.session_state:
 #     st.session_state.password = None
 
-ROLES = [None,"Coach Gus", "Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10", "Player 11", "Player 12", "Player 13", "Player 14", "Player 15", "Player 16", "Player 17", "Player 18", "Player 19", "Player 20"]
-allroles = ["Coach Gus", "Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10", "Player 11", "Player 12", "Player 13", "Player 14", "Player 15", "Player 16", "Player 17", "Player 18", "Player 19", "Player 20"]
+ROLES = [None,"Coach Gus", "Max", "Edgar", "Gaspar", "Lilly", "Colton", "Ja'zyrion", "Saboji", "Player 8", "Player 9", "Player 10", "Player 11", "Player 12", "Player 13", "Player 14", "Player 15", "Player 16", "Player 17", "Player 18", "Player 19", "Player 20"]
+allroles = ["Coach Gus", "Max", "Edgar", "Gaspar", "Lilly", "Colton", "Ja'zyrion", "Saboji", "Player 8", "Player 9", "Player 10", "Player 11", "Player 12", "Player 13", "Player 14", "Player 15", "Player 16", "Player 17", "Player 18", "Player 19", "Player 20"]
 playersdeployed = ["Coach Gus"]
 def login():
 
